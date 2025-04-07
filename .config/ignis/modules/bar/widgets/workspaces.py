8b28dd9e5f08f2bbb1de0f37269e84433612ca3c
ca1 <ca1@ca1.dev>
2025-04-07 15:31:15 +0100

    update ignis workspaces widget

--- .config/ignis/modules/bar/widgets/workspaces.py
+++ .config/ignis/modules/bar/widgets/workspaces.py
@@ -7,47 +7,43 @@ hyprland = HyprlandService.get_default()
 
 class WorkspaceButton(Widget.Button):
     def __init__(self, workspace: dict, css_classes: list[str] = [], **kwargs) -> None:
-        self.workspace = workspace
-        self.workspace_id = workspace["id"]
+        self.id = workspace.id
 
         self.icon = Widget.Box(
-            css_classes=hyprland.bind(
-                "active_window",
-                lambda _:
-                    ["bar-workspace-button-icon", "non-empty"]
-                    if self.workspace["windows"] > 0 else
-                    ["bar-workspace-button-inner", "empty"]
-            ),
+            css_classes=hyprland.bind_many(
+                ["windows", "active_window"],
+                transform=lambda *_: ["bar-workspace-button-inner", "non-empty"] if len(
+                    hyprland.get_windows_on_workspace(self.id)) > 0 else ["bar-workspace-button-inner", "empty"]
+            )
         )
 
         super().__init__(
             halign="start",
             valign="center",
             child=self.icon,
-            css_classes=hyprland.bind(
-                "workspaces",
-                lambda _:
-                    css_classes + ["active", "bar-workspace-button"]
-                    if hyprland.active_workspace["id"] == self.workspace_id
-                    else css_classes + ["inactive", "bar-workspace-button"]
+            css_classes=hyprland.bind_many(
+                ["active_workspace"],
+                transform=lambda *_: css_classes +
+                ["active", "bar-workspace-button"] if hyprland.active_workspace.id == self.id else css_classes +
+                ["inactive", "bar-workspace-button"]
             ),
             **kwargs,
         )
 
     def on_click(self, _) -> None:
         globals.currentMenu.value = ""
-        hyprland.switch_to_workspace(self.workspace_id)
+        hyprland.switch_to_workspace(self.id)
 
 
 def workspace_next() -> None:
-    current = hyprland.active_workspace["id"]
+    current = hyprland.active_workspace.id
     target = current + 1
     if target != 10:
         hyprland.switch_to_workspace(target)
 
 
 def workspace_prev() -> None:
-    hyprland.switch_to_workspace(hyprland.active_workspace["id"] - 1)
+    hyprland.switch_to_workspace(hyprland.active_workspace.id - 1)
 
 
 def Workspaces(css_classes: list[str] = [], **kwargs) -> Widget.EventBox:
@@ -55,11 +51,12 @@ def Workspaces(css_classes: list[str] = [], **kwargs) -> Widget.EventBox:
         box = Widget.EventBox(
             on_scroll_up=lambda _: workspace_next(),
             on_scroll_down=lambda _: workspace_prev(),
+
             child=hyprland.bind(
                 "workspaces",
-                transform=lambda value: [
-                    WorkspaceButton(i) for i in value
-                ],
+                transform=lambda workspaces: [
+                    WorkspaceButton(i) for i in workspaces
+                ]
             ),
             css_classes=css_classes + ["bar-widget", "bar-workspaces"],
             **kwargs,
